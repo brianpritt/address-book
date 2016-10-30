74d23b2bc7435e73e14cab60a348bc6993241505
Brian Pritt <brianpritt@gmail.com>
2016-10-30 16:30:59 -0700

    add initial files, and add code for basic funtionality of address book

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+//business logic
+function Contact(first, last) {
+  this.firstName = first;
+  this.lastName = last;
+}
+
+// user interface logic
+$(document).ready(function() {
+  $("form#new-contact").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedFirstName = $("input#new-first-name").val();
+    var inputtedLastName = $("input#new-last-name").val();
+
+    var newContact = new Contact(inputtedFirstName, inputtedLastName);
+
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
+
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+
+    $(".contact").last().click(function() {
+  $("#show-contact").show();
+  $("#show-contact h2").text(newContact.firstName);
+  $(".first-name").text(newContact.firstName);
+  $(".last-name").text(newContact.lastName);
+});
+  });
+});
