25f8ac40f593afc00f3f7d61e1db1c60db80cd39
Brian Pritt <brianpritt@gmail.com>
2016-10-30 21:17:29 -0700

    add prototype and make full name  visible in index

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,9 @@ function Contact(first, last) {
   this.firstName = first;
   this.lastName = last;
 }
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+}
 
 // user interface logic
 $(document).ready(function() {
@@ -14,16 +17,16 @@ $(document).ready(function() {
 
     var newContact = new Contact(inputtedFirstName, inputtedLastName);
 
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
 
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
 
     $(".contact").last().click(function() {
-  $("#show-contact").show();
-  $("#show-contact h2").text(newContact.firstName);
-  $(".first-name").text(newContact.firstName);
-  $(".last-name").text(newContact.lastName);
-});
+      $("#show-contact").show();
+      $("#show-contact h2").text(newContact.firstName);
+      $(".first-name").text(newContact.firstName);
+      $(".last-name").text(newContact.lastName);
+    });
   });
 });
